4a40c91973f91c49b034edfd2c991c3e548bbf66
Alexander <tikhonex@gmail.com>
2013-04-26 22:27:15 +0300

    upload

---
+++ RGhostParser/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace RGhostParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_start = new System.Windows.Forms.Button();
+            this.textBox_start = new System.Windows.Forms.TextBox();
+            this.textBox_end = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox_logs = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_start.Location = new System.Drawing.Point(18, 70);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(206, 28);
+            this.button_start.TabIndex = 0;
+            this.button_start.Text = "Parse!";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_start
+            // 
+            this.textBox_start.Location = new System.Drawing.Point(18, 34);
+            this.textBox_start.Name = "textBox_start";
+            this.textBox_start.Size = new System.Drawing.Size(100, 20);
+            this.textBox_start.TabIndex = 1;
+            // 
+            // textBox_end
+            // 
+            this.textBox_end.Location = new System.Drawing.Point(124, 34);
+            this.textBox_end.Name = "textBox_end";
+            this.textBox_end.Size = new System.Drawing.Size(100, 20);
+            this.textBox_end.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 331);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(655, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // textBox_logs
+            // 
+            this.textBox_logs.Location = new System.Drawing.Point(6, 19);
+            this.textBox_logs.Multiline = true;
+            this.textBox_logs.Name = "textBox_logs";
+            this.textBox_logs.Size = new System.Drawing.Size(381, 288);
+            this.textBox_logs.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 119);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.Text = "RGhost.ru";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(97, 119);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "zalil.ru (beta)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_start);
+            this.groupBox1.Controls.Add(this.textBox_end);
+            this.groupBox1.Controls.Add(this.button_start);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 152);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_logs);
+            this.groupBox2.Location = new System.Drawing.Point(279, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(394, 313);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Logs";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 170);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(244, 80);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Statistics";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Files found:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Processed links:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(679, 363);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RGhostParser ver. 1.2 by Tikhonex";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.TextBox textBox_start;
+        private System.Windows.Forms.TextBox textBox_end;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox_logs;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RGhostParser/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace RGhostParser
+{
+    public partial class Form1 : Form
+    {
+        int i,s=0,f=0;
+        string link,mask;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int start = int.Parse(textBox_start.Text);
+                int end = int.Parse(textBox_end.Text);
+                for (i = start; i <= end; i += 1)
+                {
+                    try
+                    {
+                        if (radioButton1.Checked)
+                        {
+                            link = "http://rghost.ru/";
+                            mask = "<li data-share42-locale='ru' data-share42-text='(.*?)' id='share42'></li>";
+                        }
+                        if (radioButton2.Checked)
+                        {
+                            link = "http://zalil.ru/";
+                            mask = "(.*?)&nbsp;&nbsp;&nbsp;(.*?)<br><br>";
+                        }
+                        progressBar1.Minimum = start;
+                        progressBar1.Maximum = end;
+                        progressBar1.Value = i;
+                        button_start.Enabled = false;
+                        if (i == end)
+                            button_start.Enabled = true;
+                        int url = i;
+                        s += 1;
+                        label5.Text = Convert.ToString(s);
+                        WebClient web = new WebClient();
+                        web.Encoding = System.Text.Encoding.UTF8;
+                        web.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.27 (KHTML, like Gecko) Chrome/26.0.1386.0 Safari/537.27");
+                        web.Headers.Add("Accept", "*/*");
+                        web.Headers.Add("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+                        web.Headers.Add("Accept-Charset", "windows-1251,utf-8;q=0.7,*;q=0.3");
+                        String html = web.DownloadString(link + Convert.ToString(url));
+                        MatchCollection m1 = Regex.Matches(html, @mask);
+                        if (radioButton1.Checked)
+                        {
+                            foreach (Match m in m1)
+                            {
+                                textBox_logs.Text += (m.Groups[1].Value + " - rghost.ru/" + Convert.ToString(url) + Environment.NewLine);
+                                f++;
+                                label6.Text = Convert.ToString(f);
+                                break;
+                            }
+                        }
+                        if (radioButton2.Checked)
+                        {
+                            foreach (Match m in m1)
+                            {
+                                textBox_logs.Text += (m.Groups[1].Value + " (" + m.Groups[2].Value + ") - zalil.ru/" + Convert.ToString(url) + Environment.NewLine);
+                                f++;
+                                label6.Text = Convert.ToString(f);
+                                break;
+                            }
+                        }
+                    }
+                    catch (WebException)
+                    {
+                        //MessageBox.Show("403 ошибка");
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ошибка ввода!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_start.Text = "34369440";
+            textBox_end.Text = "34369458";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            s = 0; label6.Text = "0";
+            f = 0; label5.Text = "0";
+            textBox_logs.Text = "";
+        }
+    }
+}
